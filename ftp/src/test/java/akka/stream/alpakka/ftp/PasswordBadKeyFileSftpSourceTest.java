2ebe1988e147df75be37f719f437be4f95b63285
Nick Faulkner <nick.faulkner@moneyfarm.com>
2018-08-31 16:51:20 +0100

    scalafmt fixes

--- ftp/src/test/java/akka/stream/alpakka/ftp/PasswordBadKeyFileSftpSourceTest.java
+++ ftp/src/test/java/akka/stream/alpakka/ftp/PasswordBadKeyFileSftpSourceTest.java
@@ -4,23 +4,24 @@
 
 package akka.stream.alpakka.ftp;
 
-        import java.net.InetAddress;
+import java.net.InetAddress;
 
 public class PasswordBadKeyFileSftpSourceTest extends SftpSourceTest {
 
   SftpSettings settings() throws Exception {
     // #create-settings
     final SftpSettings settings =
-            SftpSettings.create(InetAddress.getByName("localhost"))
-                    .withPort(getPort())
-                    .withCredentials(
-                            new FtpCredentials.NonAnonFtpCredentials(
-                                    // SftpSupportImpl validates any matching username and password as valid
-                                    "user-pass-match", "user-pass-match"))
-                    .withStrictHostKeyChecking(false) // strictHostKeyChecking
-                    .withSftpIdentity(
-                            SftpIdentity.createFileSftpIdentity(
-                                    getClientPrivateKeyFile().getPath(), "not-the-correct-key-passphrase".getBytes()));
+        SftpSettings.create(InetAddress.getByName("localhost"))
+            .withPort(getPort())
+            .withCredentials(
+                new FtpCredentials.NonAnonFtpCredentials(
+                    // SftpSupportImpl validates any matching username and password as valid
+                    "user-pass-match", "user-pass-match"))
+            .withStrictHostKeyChecking(false) // strictHostKeyChecking
+            .withSftpIdentity(
+                SftpIdentity.createFileSftpIdentity(
+                    getClientPrivateKeyFile().getPath(),
+                    "not-the-correct-key-passphrase".getBytes()));
     // #create-settings
     return settings;
   }
--- ftp/src/test/java/akka/stream/alpakka/ftp/PasswordSftpSourceTest.java
+++ ftp/src/test/java/akka/stream/alpakka/ftp/PasswordSftpSourceTest.java
@@ -11,13 +11,13 @@ public class PasswordSftpSourceTest extends SftpSourceTest {
   SftpSettings settings() throws Exception {
     // #create-settings
     final SftpSettings settings =
-            SftpSettings.create(InetAddress.getByName("localhost"))
-                    .withPort(getPort())
-                    .withCredentials(
-                            new FtpCredentials.NonAnonFtpCredentials(
-                                    // SftpSupportImpl validates any matching username and password as valid
-                                    "user-pass-match", "user-pass-match"))
-                    .withStrictHostKeyChecking(false); // strictHostKeyChecking
+        SftpSettings.create(InetAddress.getByName("localhost"))
+            .withPort(getPort())
+            .withCredentials(
+                new FtpCredentials.NonAnonFtpCredentials(
+                    // SftpSupportImpl validates any matching username and password as valid
+                    "user-pass-match", "user-pass-match"))
+            .withStrictHostKeyChecking(false); // strictHostKeyChecking
     // #create-settings
     return settings;
   }
