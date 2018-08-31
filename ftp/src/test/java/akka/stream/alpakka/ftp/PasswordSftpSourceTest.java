41413e716e02ddb485aa46527305c62c9199f534
Nick Faulkner <nick.faulkner@moneyfarm.com>
2018-08-31 16:32:50 +0100

    Test password-only SFTP auth

---
+++ ftp/src/test/java/akka/stream/alpakka/ftp/PasswordSftpSourceTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2016-2018 Lightbend Inc. <http://www.lightbend.com>
+ */
+
+package akka.stream.alpakka.ftp;
+
+import java.net.InetAddress;
+
+public class PasswordSftpSourceTest extends SftpSourceTest {
+
+  SftpSettings settings() throws Exception {
+    // #create-settings
+    final SftpSettings settings =
+            SftpSettings.create(InetAddress.getByName("localhost"))
+                    .withPort(getPort())
+                    .withCredentials(
+                            new FtpCredentials.NonAnonFtpCredentials(
+                                    // SftpSupportImpl validates any matching username and password as valid
+                                    "user-pass-match", "user-pass-match"))
+                    .withStrictHostKeyChecking(false); // strictHostKeyChecking
+    // #create-settings
+    return settings;
+  }
+}
