4baa9fb380b77ee9e28305ab7ce13eff91530644
Nick Faulkner <nick.faulkner@moneyfarm.com>
2018-08-31 10:01:38 +0100

    Push common SftpSource test boilerplate into parent class

--- ftp/src/test/java/akka/stream/alpakka/ftp/KeyFileSftpSourceTest.java
+++ ftp/src/test/java/akka/stream/alpakka/ftp/KeyFileSftpSourceTest.java
@@ -4,51 +4,11 @@
 
 package akka.stream.alpakka.ftp;
 
-import akka.NotUsed;
-import akka.stream.IOResult;
-import akka.stream.alpakka.ftp.javadsl.Sftp;
-import akka.stream.javadsl.Sink;
-import akka.stream.javadsl.Source;
-import akka.util.ByteString;
-import org.junit.Test;
 import java.net.InetAddress;
-import java.util.concurrent.CompletionStage;
-import java.util.function.Function;
 
-public class KeyFileSftpSourceTest extends SftpSupportImpl implements CommonFtpStageTest {
+public class KeyFileSftpSourceTest extends SftpSourceTest {
 
-  @Test
-  public void listFiles() throws Exception {
-    CommonFtpStageTest.super.listFiles();
-  }
-
-  @Test
-  public void fromPath() throws Exception {
-    CommonFtpStageTest.super.fromPath();
-  }
-
-  public Source<FtpFile, NotUsed> getBrowserSource(String basePath) throws Exception {
-    return Sftp.ls(basePath, settings());
-  }
-
-  public Source<ByteString, CompletionStage<IOResult>> getIOSource(String path) throws Exception {
-    return Sftp.fromPath(path, settings());
-  }
-
-  public Sink<ByteString, CompletionStage<IOResult>> getIOSink(String path) throws Exception {
-    return Sftp.toPath(path, settings());
-  }
-
-  public Sink<FtpFile, CompletionStage<IOResult>> getRemoveSink() throws Exception {
-    return Sftp.remove(settings());
-  }
-
-  public Sink<FtpFile, CompletionStage<IOResult>> getMoveSink(
-      Function<FtpFile, String> destinationPath) throws Exception {
-    return Sftp.move(destinationPath, settings());
-  }
-
-  private SftpSettings settings() throws Exception {
+  SftpSettings settings() throws Exception {
     // #create-settings
     final SftpSettings settings =
         SftpSettings.create(InetAddress.getByName("localhost"))
@@ -63,4 +23,5 @@ public class KeyFileSftpSourceTest extends SftpSupportImpl implements CommonFtpS
     // #create-settings
     return settings;
   }
+
 }
---
+++ ftp/src/test/java/akka/stream/alpakka/ftp/SftpSourceTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2016-2018 Lightbend Inc. <http://www.lightbend.com>
+ */
+
+package akka.stream.alpakka.ftp;
+
+import akka.NotUsed;
+import akka.stream.IOResult;
+import akka.stream.alpakka.ftp.javadsl.Sftp;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import akka.util.ByteString;
+import org.junit.Test;
+
+import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
+
+abstract class SftpSourceTest extends SftpSupportImpl implements CommonFtpStageTest {
+
+  @Test
+  public void listFiles() throws Exception {
+    CommonFtpStageTest.super.listFiles();
+  }
+
+  @Test
+  public void fromPath() throws Exception {
+    CommonFtpStageTest.super.fromPath();
+  }
+
+  public Source<FtpFile, NotUsed> getBrowserSource(String basePath) throws Exception {
+    return Sftp.ls(basePath, settings());
+  }
+
+  public Source<ByteString, CompletionStage<IOResult>> getIOSource(String path) throws Exception {
+    return Sftp.fromPath(path, settings());
+  }
+
+  public Sink<ByteString, CompletionStage<IOResult>> getIOSink(String path) throws Exception {
+    return Sftp.toPath(path, settings());
+  }
+
+  public Sink<FtpFile, CompletionStage<IOResult>> getRemoveSink() throws Exception {
+    return Sftp.remove(settings());
+  }
+
+  public Sink<FtpFile, CompletionStage<IOResult>> getMoveSink(
+      Function<FtpFile, String> destinationPath) throws Exception {
+    return Sftp.move(destinationPath, settings());
+  }
+
+  abstract SftpSettings settings() throws Exception;
+}
